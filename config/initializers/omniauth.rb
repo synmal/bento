15613df2f525c8becfeb2636e8c2fd14ea0aa5ac
synmal <malcolmjeremiahrajeli@gmail.com>
2019-01-29 15:45:35 +0800

    Add omniauth-github gem and configure in omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET']
+    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
    end
    

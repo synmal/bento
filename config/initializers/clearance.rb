6788980428ed625d1c674d6d37a82fb4c0ba120e
sampraslee <sampras.lee8@gmail.com>
2019-02-12 14:07:05 +0800

    Styled the About page

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -3,4 +3,5 @@ Clearance.configure do |config|
   config.mailer_sender = "reply@example.com"
   config.rotate_csrf_on_sign_in = true
 
+  Clearance::PasswordController.layout 'my_passwords_layout'
 end

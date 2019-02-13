463969588938089868e817189ddf66f6862ce4a2
synmal <malcolmjeremiahrajeli@gmail.com>
2019-02-13 08:27:11 +0800

    fic

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
 
-	def newsletter(user, article, podcast, video, project)
+	def newsletter(user)
       @user = user
-      @article = article
-      @podcast = podcast
-      @video = video
-      @project = project
+      @article = @user.feeds.last.article if user.feeds.last.article
+      @podcast = @user.feeds.last.podcast if user.feeds.last.podcast
+      @video = @user.feeds.last.video
+      @project = @user.feeds.last.project
 	    mail(to: @user.email, subject: 'Your Weekly Bento is Here')
 	end
 

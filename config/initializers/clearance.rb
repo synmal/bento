10cca7ab7c7382df6a55da14f6cc3584d5ffc0db
sampraslee <sampras.lee8@gmail.com>
2019-02-13 08:34:29 +0800

    dont know

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -2,9 +2,4 @@ Clearance.configure do |config|
   config.routes = false
   config.mailer_sender = "bento@bentonext.com"
   config.rotate_csrf_on_sign_in = true
-<<<<<<< HEAD
-  # Clearance::PasswordsController.layout "my_passwords_layout"
-=======
-  # Clearance::PasswordsController.layout 'my_passwords_layout'
->>>>>>> upstream/master
 end
